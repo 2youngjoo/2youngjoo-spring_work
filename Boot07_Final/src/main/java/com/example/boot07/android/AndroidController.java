5eed4eb02f8d2f0ee870e2034fec0ef6fcf32b16
kimgura <krkrdudwn@naver.com>
2023-07-24 12:55:15 +0900

    boot07 android 테스트 controller 추가

---
+++ Boot07_Final/src/main/java/com/example/boot07/android/AndroidController.java
@@ -0,0 +1,41 @@
+package com.example.boot07.android;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Responsebody가 포함된 컨트롤러
+@RestController
+public class AndroidController {
+	
+	@GetMapping("/android/tweet")
+	public String tweet(String msg) {
+		System.out.println("안드로이드에서 전송된 문자열:"+msg);
+		return "success!";
+	}
+	
+	@PostMapping("/android/tweet2")
+	public Map<String, Object> tweet2(String msg){
+		System.out.println("안드로이드에서 전송된 문자열:"+msg);
+		Map<String, Object> map=new HashMap<>();
+		map.put("isSuccess", true);
+		return map;
+	}
+	
+	@GetMapping("/android/tweet3")
+	public List<String> tweet3(String msg){
+		System.out.println("안드로이드에서 전송된 문자열:"+msg);
+		
+		List<String> names=new ArrayList<String>();
+		names.add("김구라");
+		names.add("해골");
+		names.add("원숭이");
+		
+		return names;
+	}
+}
